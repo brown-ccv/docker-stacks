416da47613b973fc4e8602fa3cdfd970212f3fca
Isabel Restrepo <isabelrestre@gmail.com>
2018-12-03 15:35:46 -0500

    Remove python related packages from julia_base

--- julia_base/plotting_pkgs.jl
+++ julia_base/plotting_pkgs.jl
@@ -1,6 +1,9 @@
 const plotting_pkgs = [ "Gadfly",
+                        "GR"
                         "Plots",
-                        "VegaLite"]
+                        "VegaLite",
+                        "StatPlots",
+                        "PlotRecipes"]
 
 # "PlotlyJS", "DataVoyager" - failed ... Electron failed to build
 # │ ERROR: LoadError: IOError: could not spawn `unzip -q electron-v1.8.4-linux-x64.zip -d electron`: no such file or directory (ENOENT)
