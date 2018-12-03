6d25913336b4fe7e693a6192b670a33047dde5a9
Isabel Restrepo <isabelrestre@gmail.com>
2018-12-03 10:48:54 -0500

    configure depot path at entrypoint

--- julia_base/entrypoint.sh
+++ julia_base/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-export 
+# This adds the home directory to depot path - 
+# so that julia cache files are go into writable location
+export JULIA_DEPOT_PATH=${HOME}:${JULIA_DEPOT_PATH}
