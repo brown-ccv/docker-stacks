32648d0f3d59fda0b6992baf37150c3feadf3f05
Isabel Restrepo <isabelrestre@gmail.com>
2018-12-04 22:06:50 -0500

    removed procompiled libraries, move scripts out of /tmp/

--- entrypoint_scripts/julia_base.sh
+++ entrypoint_scripts/julia_base.sh
@@ -2,5 +2,8 @@
 
 # This adds the home directory to depot path - 
 # so that julia cache files are go into writable location
+rm -rf $JULIA_DEPOT_PATH/compiled
 export JULIA_DEPOT_PATH="$HOME/.julia:$JULIA_DEPOT_PATH"
+
+# Pass all arguments
 exec "$@"
