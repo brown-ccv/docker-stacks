3a3d3dfba4ac66f30accc56560319214230b8a4e
fernandogelin <fernandogelin@gmail.com>
2019-04-02 17:11:48 -0400

    feat(mlj): add mlj image, mlj list of packages

--- stronghold/julia_scripts/install_packages.jl
+++ stronghold/julia_scripts/install_packages.jl
@@ -17,13 +17,30 @@ function install(pkgs)
 end
 
 """
+clone(pkgs)
+Call `Pkg.clone` on list of desired packages
+"""
+function clone(pkgs::Array)
+    for pkg in pkgs
+        println("--------------------------------")
+        println("Package: ", pkg)
+        println("--------------------------------")
+        Pkg.clone("https://github.com/$pkg")
+    end
+end
+
+"""
 precompile()
 Run `using` an all lists of packages
 """
 function precompile(pkgs)
     failed_pkgs = Vector{String}()
 
-    for pkg in pkgs
+    for p in pkgs
+        try
+            pkg = split(p, "/")[2]
+        catch
+            pkg = p
         println("--------------------------------")
         println("Using Package: ", pkg)
         println("--------------------------------")
@@ -32,7 +49,7 @@ function precompile(pkgs)
             eval(:(using $pkgsym))
         catch
             warn("using pkg failed")
-            push!(failed_pkgs, pkg)
+            push!(failed_pkgs, p)
         end
     end
     println("--------------------------------")
@@ -41,4 +58,17 @@ function precompile(pkgs)
     println("--------------------------------")
 
     return failed_pkgs
+end
+
+function precompile(pkgs)
+    failed_pkgs = Vector{String}()
+
+    for pkg in pkgs
+        try
+            pkg = split(pkg, "/")[2]
+        catch
+            pkg = pkg
+        end
+        println(pkg)
+    end
 end
