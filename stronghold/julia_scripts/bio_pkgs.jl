27cec02d07bd32d738b2b7fc1893f99c3d62779b
fernandogelin <fernandogelin@gmail.com>
2019-05-28 10:52:42 -0400

    fix(bio): add blink

--- stronghold/julia_scripts/bio_pkgs.jl
+++ stronghold/julia_scripts/bio_pkgs.jl
@@ -4,7 +4,6 @@ const bio_pkgs = [   "ArgParse",
                      "BioAlignments",
                      "BioStructures",
                      "GeneticVariation",
-                     "Rsvg",
-                     "PlotlyJS",
+                     "Blink",
                      "VariantVisualization#master"
                     ]
