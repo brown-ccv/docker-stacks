77be2f3980b22cbc2260b7e1a4d2ac91cc96698f
fernandogelin <fernandogelin@gmail.com>
2019-06-18 13:03:52 -0400

    feat: add more plotting packages to base julia

--- stronghold/julia_scripts/plotting_pkgs.jl
+++ stronghold/julia_scripts/plotting_pkgs.jl
@@ -2,7 +2,12 @@ const plotting_pkgs = [ "Gadfly",
                         "GR",
                         "Plots",
                         "VegaLite",
-                        "StatPlots"]
+                        "StatPlots",
+                        "Plotly",
+                        "PlotlyJS",
+                        "UnicodePlots",
+                        "StatsPlots",
+                        "PlotsRecipes"]
 
 # "PlotlyJS", "DataVoyager" - failed ... Electron failed to build
 # │ ERROR: LoadError: IOError: could not spawn `unzip -q electron-v1.8.4-linux-x64.zip -d electron`: no such file or directory (ENOENT)
