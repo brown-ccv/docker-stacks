6dd83304ef1c985f4781f0772fda6e30adb1a45c
fernandogelin <fernandogelin@gmail.com>
2019-07-09 09:27:50 -0400

    fix: add zmq and blosc

--- stronghold/julia_scripts/external_pkgs.jl
+++ stronghold/julia_scripts/external_pkgs.jl
@@ -1,6 +1,6 @@
 println("Python")
 ENV["PYTHON"]=""
-const external_pkgs = ["PyCall", "PyPlot", "IJulia", "ScikitLearn", "Seaborn", "Pandas"]
+const external_pkgs = ["PyCall", "PyPlot", "Blosc", "ZMQ", "IJulia", "ScikitLearn", "Seaborn", "Pandas"]
 
 # "RCall" Error: R_HOME not a directory
 # "Seaborn" ERROR: UndefVarError: warn not defined
