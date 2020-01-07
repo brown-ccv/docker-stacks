302b3376d7853e9c8721361ea5aef702f1bf088c
fernandogelin <fernando@fernandogelin.com>
2020-01-07 14:03:08 -0500

    bug

--- jupyterhub/scripts/install_sys_deps.sh
+++ jupyterhub/scripts/install_sys_deps.sh
@@ -4,15 +4,9 @@
 DEBIAN_FRONTEND=noninteractive
 apt-get update && apt-get -yq dist-upgrade \
 && apt-get install -yq --no-install-recommends \
-curl \
-dirmngr \
-apt-transport-https \
-lsb-release \
-ca-certificates \
 openssh-client \
 vim \
 curl \
 git-lfs \
-&& apt-get clean \
-&& curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - \
-&& apt -yq install nodejs npm
+nodejs \
+&& apt-get clean
